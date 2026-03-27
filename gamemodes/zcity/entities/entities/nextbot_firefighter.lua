201c43eda192d3109e5a5c6033d37e5e65ffdadf
jmd7231 <jad3221@gmail.com>
2026-03-27 17:53:12 -0400

    Fix firefighter model/animation and extinguish behavior

--- gamemodes/zcity/entities/entities/nextbot_firefighter.lua
+++ gamemodes/zcity/entities/entities/nextbot_firefighter.lua
@@ -4,7 +4,7 @@ ENT.Base = "base_nextbot"
 ENT.Spawnable = false
 ENT.PrintName = "Firefighter NextBot"
 
-ENT.Model = "models/player/riot.mdl"
+ENT.Model = "models/player/Group03/male_07.mdl"
 ENT.SearchRadius = 25000
 ENT.MoveSpeed = 700
 ENT.ExtinguishRange = 175
@@ -29,21 +29,18 @@ function ENT:Initialize()
     
     if CLIENT then return end
 
-    if not self.loco then
-        ErrorNoHalt("[nextbot_firefighter] Missing loco in Initialize; removing bot\n")
-        self:Remove()
-        return
+    if self.loco then
+        self.loco:SetDesiredSpeed(self.MoveSpeed)
+        self.loco:SetAcceleration(4000)
+        self.loco:SetDeceleration(4000)
+        self.loco:SetStepHeight(32)
+        self.loco:SetJumpHeight(150)
     end
-
-    self.loco:SetDesiredSpeed(self.MoveSpeed)
-    self.loco:SetAcceleration(4000)
-    self.loco:SetDeceleration(4000)
-    self.loco:SetStepHeight(32)
-    self.loco:SetJumpHeight(150)
 
     self.NextTargetSearch = 0
     self.NextSpray = 0
     self.LastFireSeen = CurTime()
+    self.LastMoveThink = CurTime()
 
     self:EquipExtinguisherProp()
 end
@@ -97,64 +94,95 @@ function ENT:FindBestFire()
 end
 
 function ENT:RunBehaviour()
-    if not self.loco then return end
-
     while true do
-        self:StartActivity(ACT_RUN)
-
-        if self.NextTargetSearch <= CurTime() then
-            self.NextTargetSearch = CurTime() + self.TargetRefreshDelay
-            self.TargetFire = self:FindBestFire()
-        end
-
-        if IsValid(self.TargetFire) then
-            self.LastFireSeen = CurTime()
-            self:MoveAndExtinguish(self.TargetFire)
-        elseif self.LastFireSeen + self.DespawnDelay <= CurTime() then
-            self:Remove()
-            return
-        else
-            self:StartActivity(ACT_IDLE)
-            coroutine.wait(0.1)
-        end
-
+        coroutine.wait(0.1)
         coroutine.yield()
     end
 end
 
-function ENT:MoveAndExtinguish(fire)
-    if not self.loco then return end
+function ENT:MoveTowards(pos, delta)
+    local myPos = self:GetPos()
+    local toGoal = pos - myPos
+    local dist = toGoal:Length()
+    if dist <= 1 then return dist end
+
+    local moveDir = toGoal / dist
+    local moveAng = moveDir:Angle()
+    self:SetAngles(Angle(0, moveAng.y, 0))
+
+    local step = math.min(dist, self.MoveSpeed * delta)
+    local nextPos = myPos + moveDir * step
+
+    local tr = util.TraceHull({
+        start = myPos + Vector(0, 0, 20),
+        endpos = nextPos + Vector(0, 0, 20),
+        mins = Vector(-12, -12, 0),
+        maxs = Vector(12, 12, 64),
+        filter = self,
+        mask = MASK_PLAYERSOLID
+    })
+
+    if tr.Hit then
+        if IsValid(tr.Entity) and hgIsDoor and hgIsDoor(tr.Entity) and tr.Entity:GetInternalVariable("m_eDoorState") == 0 then
+            tr.Entity:Fire("Open")
+        else
+            nextPos = myPos + Vector(0, 0, 24)
+        end
+    end
+
+    self:SetPos(nextPos)
+    return dist
+end
+
+function ENT:SprayAtFire(fire)
+    if self.NextSpray > CurTime() then return end
+    self.NextSpray = CurTime() + 0.1
+
+    self:EmitSound("fire_extinguisher/fire_extinguisger_startloop.wav", 65, 100, 0.6, CHAN_AUTO)
 
     local firePos = fire:GetPos()
-    local myPos = self:GetPos()
-    local toFire = firePos - myPos
-    local dist = toFire:Length()
+    for _, ent in ipairs(ents.FindInSphere(firePos, self.ExtinguishRange * 0.75)) do
+        local class = ent:GetClass()
+        if class == "vfire" or class == "vfire_ball" then
+            ent:Extinguish()
+        end
+    end
+end
 
-    if dist > 1 then
-        local moveDir = toFire / dist
+function ENT:Think()
+    if CLIENT then return end
 
-        self:SetAngles(moveDir:Angle())
-        self.loco:SetVelocity(moveDir * self.MoveSpeed)
+    local curTime = CurTime()
+    local delta = math.Clamp(curTime - self.LastMoveThink, 0, 0.1)
+    self.LastMoveThink = curTime
 
-        local tr = util.TraceLine({
-            start = myPos + Vector(0, 0, 36),
-            endpos = firePos + Vector(0, 0, 36),
-            filter = self,
-            mask = MASK_SOLID_BRUSHONLY
-        })
+    if self.NextTargetSearch <= curTime then
+        self.NextTargetSearch = curTime + self.TargetRefreshDelay
+        self.TargetFire = self:FindBestFire()
+    end
 
-        if tr.Hit and self:IsOnGround() then
-            self.loco:Jump()
+    if IsValid(self.TargetFire) then
+        self.LastFireSeen = curTime
+        self:StartActivity(ACT_RUN)
+
+        local dist = self:MoveTowards(self.TargetFire:GetPos(), delta)
+        if dist <= self.ExtinguishRange then
+            self:SprayAtFire(self.TargetFire)
+        end
+    else
+        self:StartActivity(ACT_IDLE)
+        if self.LastFireSeen + self.DespawnDelay <= curTime then
+            self:Remove()
+            return
         end
     end
 
-    if dist <= self.ExtinguishRange and self.NextSpray <= CurTime() then
-        self.NextSpray = CurTime() + 0.1
+    self:NextThink(curTime)
+    return true
+end
 
-        if IsValid(fire) then
-            fire:Extinguish()
-        end
-    end
+function ENT:BodyUpdate()
+    self:BodyMoveXY()
 end
 
 function ENT:OnContact(ent)
@@ -167,5 +195,7 @@ end
 
 function ENT:HandleStuck()
     self:SetPos(self:GetPos() + VectorRand() * 30 + Vector(0, 0, 20))
-    self.loco:ClearStuck()
+    if self.loco then
+        self.loco:ClearStuck()
+    end
 end
