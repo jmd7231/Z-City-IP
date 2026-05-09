969b778b19f0997988050b32e977b43031d1d740
jmd7231 <jad3221@gmail.com>
2026-05-09 14:41:17 -0400

    Add shrinking forcefield behavior to Magic DM

--- gamemodes/zcity/gamemode/modes/magicdm/cl_magicdm.lua
+++ gamemodes/zcity/gamemode/modes/magicdm/cl_magicdm.lua
@@ -2,6 +2,9 @@ MODE.name = "magicdm"
 
 local MODE = MODE
 
+local deathmatch_nozone = ConVarExists("deathmatch_nozone") and GetConVar("deathmatch_nozone") or CreateConVar("deathmatch_nozone", 0, FCVAR_REPLICATED, "Allows to disable deathmatch mode zone.", 0, 1)
+local mat = Material("hmcd_dmzone")
+
 local fighter = {
 	objective = "Kill everyone with magic.",
 	name = "Fighter",
@@ -19,4 +22,24 @@ function MODE:HUDPaint()
 	colorRole.a = 255 * fade
 	draw.SimpleText("You are a " .. fighter.name, "ZB_HomicideMediumLarge", sw * 0.5, sh * 0.5, colorRole, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 	draw.SimpleText(fighter.objective, "ZB_HomicideMedium", sw * 0.5, sh * 0.9, colorRole, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end
+
+hook.Add("Think", "MagicDMZoneSoundThink", function()
+	if CurrentRound() and CurrentRound().name ~= "magicdm" then return end
+	local station = zb.SoundStation
+	if not IsValid(station) then return end
+	if deathmatch_nozone:GetBool() then return end
+	if not ZonePos then return end
+
+	local radius = MODE.GetZoneRadius()
+	local volume = math.Clamp((LocalPlayer():GetPos():Distance(ZonePos) - radius) + 200, 0, 200) / 200
+	station:SetVolume(volume)
+end)
+
+function MODE:PostDrawTranslucentRenderables(bDepth, bSkybox, isDraw3DSkybox)
+	if (not bSkybox and not isDraw3DSkybox) and not deathmatch_nozone:GetBool() and ZonePos then
+		local radius = MODE.GetZoneRadius()
+		render.SetMaterial(mat)
+		render.DrawSphere(ZonePos, -radius, 60, 60, color_white)
+	end
 end
--- gamemodes/zcity/gamemode/modes/magicdm/sv_magicdm.lua
+++ gamemodes/zcity/gamemode/modes/magicdm/sv_magicdm.lua
@@ -5,6 +5,8 @@ MODE.name = "magicdm"
 MODE.PrintName = "Magic DM"
 MODE.Chance = 0.04
 
+local deathmatch_nozone = ConVarExists("deathmatch_nozone") and GetConVar("deathmatch_nozone") or CreateConVar("deathmatch_nozone", 0, FCVAR_REPLICATED, "Allows to disable deathmatch mode zone.", 0, 1)
+
 function MODE:RoundStart()
 	for _, ply in player.Iterator() do
 		if not ply:Alive() then continue end
@@ -30,3 +32,26 @@ function MODE:RoundStart()
 		ply:SetNetVar("CurPluv", "pluvboss")
 	end
 end
+
+local cooldown = CurTime()
+hook.Add("Think", "magicdm_zone_shrink", function()
+	local rnd = CurrentRound()
+	if not rnd or rnd.name ~= "magicdm" then return end
+	if (zb.ROUND_START or CurTime()) + 20 > CurTime() then return end
+	if cooldown > CurTime() then return end
+	if deathmatch_nozone:GetBool() then return end
+
+	cooldown = CurTime() + 0.5
+
+	local pos = zonepoint
+	if not pos then return end
+
+	local radius = MODE.GetZoneRadius()
+	local radiusSqr = radius * radius
+
+	for _, ent in ents.Iterator() do
+		if pos:DistToSqr(ent:GetPos()) > radiusSqr and ent:IsPlayer() then
+			hg.LightStunPlayer(ent)
+		end
+	end
+end)
